4344373db9e57e71efa6c5f182b6aefb1cfe89ab
Aaron Schlegel <aaron@aaronschlegel.com>
2018-06-23 16:59:28 -0700

    remove tests folder from gitignore

---
+++ tests/testthat/test-getpoetry.R
@@ -0,0 +1,13 @@
+context('test get.poetry function')
+
+httptest::with_mock_api({
+  test_that('get.poetry single input term no search term', {
+    skip_on_cran()
+    p <- get.poetry('author')
+
+    expect_true(is.vector(p$authors))
+    expect_true('Ambrose Bierce' %in% p$authors)
+    expect_true(length(p$authors) >= 129)
+
+  })
+})
