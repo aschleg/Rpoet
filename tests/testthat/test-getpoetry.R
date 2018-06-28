5fb873380e4c831121b7ea7d2bb8c3e02327e5ac
Aaron Schlegel <aaron@aaronschlegel.com>
2018-06-28 07:35:19 -0700

    add vignettes, updated tests and cran comments file for CRAN submission

--- tests/testthat/test-getpoetry.R
+++ tests/testthat/test-getpoetry.R
@@ -11,11 +11,28 @@ httptest::with_mock_api({
     expect_true('Ambrose Bierce' %in% p$authors)
     expect_true(length(p$authors) >= 129)
 
+    w_abs <- get.poetry('author', 'William Shakespeare', 'title,linecount', search_type = 'abs')
+    w <- get.poetry('author', 'William Shakespeare', 'title,linecount')
+
+    expect_true(nrow(w) == nrow(w_abs))
+    expect_true(ncol(w) == ncol(w_abs))
+
+    expect_true(is.data.frame(w))
+
     expect_error(get.poetry('author', output_format = 'adfj'),
                  "output_format parameter must be one of 'json', 'text', or NULL.")
 
     expect_error(get.poetry('author', 'William Shakespeare', 'title', search_type = 'ab'),
                  "search_type parameter must be one of 'abs' or 'absolute' for absolute match searches or NULL for broad match searches.")
+
+  })
+
+  test_that('get.poetry combination searches return data appropriately', {
+    skip_on_cran()
+
+    w <- get.poetry('author,title', 'William Shakespeare;Spring and Winter ii', 'author,title')
+
+    expect_true(is.data.frame(w))
 
   })
 })
